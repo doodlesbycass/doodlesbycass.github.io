0fa7a9e80bc9349290997edd85b2ac57f6f847c3
doodlesbycass <doodles.by.cass@gmail.com>
2023-01-06 18:33:12 -0500

    doodles.by.cass

---
+++ repeatingElements/header.js
@@ -0,0 +1,17 @@
+document.write(
+    `<div id="header">
+        <a href="/index.html"><h1>Doodles By Cass</h1></a>
+        <div id="navIcon" onclick="showNav();"><img id="navIcon" src="navIcon.svg" alt="navigation icon"></div>
+        <div id="nav">
+            <a href="shop.html">Shop</a>
+            <a href="portfolio.html">Portfolio</a>
+        </div>
+    </div>
+    <script>
+        function showNav() {
+            var nav;
+            nav = document.getElementById("nav");
+            if (nav.style.display === "none") {nav.style.display = "block";} else {nav.style.display = "none";}
+        }
+    </script>`
+);
