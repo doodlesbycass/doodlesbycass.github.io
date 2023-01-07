7a49e05a31307719e16df6e9d4abdb34e2f136df
doodlesbycass <doodles.by.cass@gmail.com>
2023-01-06 19:12:26 -0500

    Update header.js

--- repeatingElements/header.js
+++ repeatingElements/header.js
@@ -1,17 +1,19 @@
 document.write (`
-    <div id="header" onresize="checkNav();">
+    <div id="header">
         <a href="/index.html"><h1>Doodles By Cass</h1></a>
-        <div id="navIcon" onclick="showNav();"><img id="navIcon" src="/repeatingElements/navIcon.svg" alt="navigation icon"></div>
+        <div id="navIcon"><img id="navIcon" src="/repeatingElements/navIcon.svg" alt="navigation icon"></div>
         <div id="nav">
             <a href="/shop.html">Shop</a>
             <a href="/portfolio.html">Portfolio</a>
         </div>
     </div>
     <script>
+        document.getElementById("navIcon").addEventListener("click", showNav);
+        document.getElementById("header").addEventListener("resize", checkNav);
         function showNav() {
             var nav;
             nav = document.getElementById("nav");
-            if (nav.style.display === "none") {nav.style.display = "block";} else {nav.style.display = "none";}
+            if (nav.style.display !== "block") {nav.style.display = "block";} else {nav.style.display = "none";}
         }
         function checkNav() {
             var navIcon;
